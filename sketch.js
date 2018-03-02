fc58035a394c9314b085a94dff213171cea3333b
K4en <36987725+K4en@users.noreply.github.com>
2018-03-02 12:01:40 +0000

    new files

--- sketch.js
+++ sketch.js
@@ -1,12 +1,48 @@
+
+var char;
+var char_img;
+
+function preload(){
+
+char_img = loadImage('sprites/char_sprite.png')
+
+}
+
 function setup()
 {
   createCanvas(500,500)
-  background(144,144,144)
+  
+
+  char = createSprite(width/2, height/2);
+  char.addImage(char_img);
+
 }
+
+
 
 function draw()
 {
-  var test = 1
-  test++;
-  console.log(test)
+ background(144,144,144)
+ 
+    
+    
+ char.position.x = width/2;
+ char.position.y = height/2;
+ drawSprites();
+
+}
+
+
+function Character(){
+
+//Front Standing
+
+
+//Left Standing
+
+
+//Right standing
+
+
+
 }
