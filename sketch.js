08f8f125ee9b326ef827289a5abafb8059ab5714
K4en <36987725+K4en@users.noreply.github.com>
2018-03-07 21:10:23 +0000

    Code clean up + comments

--- sketch.js
+++ sketch.js
@@ -1,68 +1,52 @@
-
+//Sprite variabless
 var warrior;
 var warrior_img;
 
 var ground;
 var ground_img;
 
-var floorPos_y;
-var tile_sheet;
-var ground_frames;
-var tile_frames;
-
+//Arrays
 var grounds = [];
 
 
-//Key booleans
+//Key/Movement booleans
 var isLeft = false;
 var isRight = false;
 var isJumping = false;
 var isFalling = false;
 
 
-//var tile_frames = [
-//  {"name":"ground.png", "frame":{"x":0, "y": 0, "width": 100, "height": 26}},
-////  {"name":"ground", "frame":{"x":100, "y": 300, "width": 100, "height": 26}},
-////  {"name":"ground", "frame":{"x":200, "y": 300, "width": 100, "height": 26}},
-////  {"name":"ground", "frame":{"x":300, "y": 300, "width": 100, "height": 26}},
-//];
-
 function preload(){
-    //tile_frames = loadJSON('tiles.json');
+
     warrior_img = loadImage('sprites/char_sprite.png');
     ground_img = loadImage('sprites/ground_sprite.png');
-
-
-    loadJSON('tiles.json', function(tile_frames) {
-        // Load tiles sprite sheet from frames array once frames array is ready
-        tile_sheet = loadSpriteSheet('sprites/ground_sheet.png', tile_frames);
-
-    });
-    //tile_sheet = loadSpriteSheet('sprites/ground_sprite.png', tile_frames);
 
 }
 
 function setup()
 {
-    createCanvas(800, 500);
-    floorPos_y = height * 3/4;
+    createCanvas(900, 500);
 
 
+    //Creating sprites
     warrior = createSprite(width/2, height/2);
     warrior.addImage(warrior_img);
-    
-    for(var x = 0; x < 800; x+= 100)
-        {
-      ground = createSprite(x, 300);
-      ground.addImage(ground_img);
-      grounds.push(ground)
-        }
-    
+
+    //Looping the ground to draw multiple sprites
+    for(var x = 0; x < 800; x+= 120)
+    {
+        //Prob can be mapped to sound
+        var groundY = random(300, 500)
+        ground = createSprite(x, groundY);
+        ground.addImage(ground_img);
+        grounds.push(ground)
+    }
+
 
     console.log(grounds)
     console.log(tile_frames)
-    
-//    ground.addToGroup(grounds);
+
+    //    ground.addToGroup(grounds);
 
 }
 
@@ -73,37 +57,37 @@ function draw()
     //    clear();
     background(144,144,144)
 
-    // tile_sheet.drawFrame('ground.png', 100, 100);
-//    for(var x = 0; x < 800; x +=100)
-//    {
-//        tile_sheet.drawFrame('ground.png', x, 300);
-//    }
-
-//    warrior.position.x = mouseX;
-//    warrior.position.y = mouseY;
-    for(var i = 0; i < grounds.length; i++)
-        {
-    warrior.collide(grounds[i]);
-        }
-    drawSprites();
     
+    //Looping through 'grounds' for collision, otherwise it only collides with last sprite.
+    for(var i = 0; i < grounds.length; i++)
+    {
+        warrior.collide(grounds[i]);
+    }
+    //Drawing all sprites
+    drawSprites();
+
+    
+    
+    //Moving logics and mirroring warrior sprite so we only use 1 sprite for now.
     if(isRight)
-        {
-            warrior.position.x += 1;
-        }
+    {
+        warrior.mirrorX(1);
+        warrior.position.x += 1;
+    }
     if(isLeft)
-        {
-            warrior.position.x -= 1;
-        }
+    {
+        warrior.mirrorX(-1);
+        warrior.position.x -= 1;
+    }
     if(isJumping)
-        {
-            warrior.position.y -= 1;
-        }
+    {
+        warrior.position.y -= 1;
+    }
     else if(isFalling)
-        {
-            
-            warrior.position.y += 2;
-        }
+    {
+
+        warrior.position.y += 2;
+    }
 
 }
 
@@ -124,34 +108,34 @@ function Character(){
 
 function keyPressed(){
     if(key == 'D')
-        {
-            isRight = true;
-//            warrior.position.x += 1;
-        }
+    {
+        isRight = true;
+        //            warrior.position.x += 1;
+    }
     if(key == 'A')
-        {
-            isLeft = true;
-        }
+    {
+        isLeft = true;
+    }
     if(key == 'W')
-        {
-            isJumping = true;
-        }
+    {
+        isJumping = true;
+    }
 }
 
 function keyReleased(){
     if(key == 'D')
-        {
-            isRight = false;
-        }
+    {
+        isRight = false;
+    }
     if(key == 'A')
-        {
-            isLeft = false;
-        }
+    {
+        isLeft = false;
+    }
     if(key == 'W')
-        {
-            isJumping = false;
-            isFalling = true;
-        
-        }
-    
+    {
+        isJumping = false;
+        isFalling = true;
+
+    }
+
 }
