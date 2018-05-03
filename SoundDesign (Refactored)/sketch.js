dcd956654ae64ef67122e18526f9b655a16368e1
K4en <36987725+K4en@users.noreply.github.com>
2018-05-03 14:27:45 +0100

    Collision improve
    Set collider for every object so now they properly collide.
    Set up a .debug on everything so it's more visual whats happening in the program.
    
    New coin collecting function(simpler)

--- SoundDesign (Refactored)/sketch.js
+++ SoundDesign (Refactored)/sketch.js
@@ -21,13 +21,17 @@ var warriorImg, groundImg, coinImg;
 var score = 0;
 var enemy_smallImg, shroomImg;
 
+var lifePoints;
+
+
 function setup()
 {
     setupSound() //This is the setup function inside the soundDesign.js function.
 
     createCanvas(innerWidth,innerHeight) //Sets the canvas to stretch the screen
 
-    
+
+
     //Preloading images for sprites
     warriorImg = loadImage("sprites/char_sprite.png");
     groundImg = loadImage("sprites/pix_ground.png");
@@ -36,31 +40,34 @@ function setup()
     platformImg = loadImage("sprites/platform_sprite.png");
     enemy_smallImg = loadImage("sprites/enemy_sprite.png");
     shroomImg = loadImage("sprites/shroom_sprite.png");
-    
+
     GROUND_Y = height - 100;
-    
+
     //Creating warrior sprite
-    warrior = createSprite(width/2, GROUND_Y -130,10,10);
-//    warrior.rotateToDirection = true;
+    warrior = createSprite(width/2, GROUND_Y -130, 47, 55);
     warrior.velocity.x = 3;
-//    warrior.setCollider("circle");
+    warrior.setCollider("rectangle");
     warrior.addImage(warriorImg);
+    warrior.debug = true;
 
-    
+
     //Creating ground sprite
-//    for(var i = 0; i < width/100; i+= 100){
-    ground = createSprite(width/2, GROUND_Y); //image 800x200
+    ground = createSprite(width/2, GROUND_Y);
     ground.addImage(groundImg);
-//    ground.scale = 0.5
     ground.width = width/4+115;
-//    grounds.push(ground)
-//    }
+    ground.setCollider("rectangle", 0, 0, 2600, 200)
+    ground.debug = true;
+    
 
+    lifePoints = 50;
+
+    //Creating groups (it's like an array for sprites)
     coins = new Group();
     grounds = new Group();
     platforms = new Group();
     enemies = new Group();
     shrooms = new Group();
+
     gameOver = true;
     updateSprites(false);
 
@@ -71,116 +78,144 @@ function draw()
 {
 
     soundDraw() //This is the draw function inside the soundDesign.js function.
-//    background(bassMapped,0,0)
-//    ellipse(500,500,bassMapped * 4, bassMapped * 4)
+    //    background(bassMapped,0,0)
+    //    ellipse(500,500,bassMapped * 4, bassMapped * 4)
 
     //If gameover and music playing
-    if(gameOver && isPlaying)
+    if(gameOver)
         newGame();
-    
+
     if(!gameOver) {
 
-    if(keyWentDown("x"))
-      warrior.velocity.y = FLAP;
-    
-    warrior.velocity.y += GRAVITY;
-    
-    if(warrior.position.y<0){
-      warrior.position.y = 0;
-    }
-     
-    //To keep him on the ground. A kind of collision detection.
-    if(warrior.position.y+warrior.height/2 > GROUND_Y-130){
-        warrior.position.y = GROUND_Y - 130;
-    }
+        if(keyWentDown("x"))
+            warrior.velocity.y = FLAP;    
+
+        warrior.velocity.y += GRAVITY;
+
+
+
+        if(warrior.position.y<0){
+            warrior.position.y = 0;
+        }
+
+        warrior.collide(ground)
         
     }
-    
-    
+
+
     //Only draws coins if the music is playing
     if(isPlaying){
-        
-        //Framecount needed to space them out evenly. For now
-    if(frameCount%60 == 0){
-        
-        //The Y position is mapped for the bass for now
-            var groundH = random(20, 160);
-            var coin = createSprite(warrior.position.x+width/2, lowMidMapped, 80, groundH);
-            coin.addImage(coinImg);
-            coins.push(coin)
-        
-        //Creating platforms under some of the coins
-                
-        var platform = createSprite(warrior.position.x+width/2+random(200,400)*random(2,4), lowMidMapped*random(3,5)+50, 100, 40);
-        platform.addImage(platformImg);
-//        platform.setCollider('rectangle');
-        platforms.push(platform);
-        
-        //Creating shrooms from camera position so its not tied to the character
-        var shroomH = random(20, 80);
-        var shroom = createSprite(camera.position.x+width/2+random(200, 400), GROUND_Y-130, 50, shroomH);
-        shroom.addImage(shroomImg);
-        shrooms.push(shroom);
-        
-        //Creating small enemies between the shrooms with a basic velocity 1;
-        var small_enemy = createSprite(shroom.position.x+random(40, 50), GROUND_Y-110);
-        small_enemy.addImage(enemy_smallImg);
-        small_enemy.velocity.x = 1;
-        enemies.push(small_enemy);
-//            console.log(platform)
-        }
-    }
-        
-        //Collecting the coins
-        for(var i = 0; i < coins.length; i++){
-        var collect = warrior.collide(coins[i]);
 
-        if(collect){
-            coins[i].remove();
-            coins.splice(i, 1);
-            score += 10;
+        //Framecount needed to space them out evenly. For now
+        if(frameCount%60 == 0){
+
+            //The Y position is mapped for the bass for now
+            var groundH = random(20, 160);
+            var coin = createSprite(warrior.position.x+width/2, lowMidMapped, 30, 30);
+            coin.addImage(coinImg);
+            coin.setCollider("circle");
+            coin.debug = true;
+            coins.push(coin)
+
+            //Creating platforms under some of the coins
+
+            var platform = createSprite(warrior.position.x+width/2+random(200,400)*random(2,4), lowMidMapped*random(3,5)+50, 99, 23);
+            platform.addImage(platformImg);
+            platform.debug = true;
+            platform.setCollider("rectangle");
+            platforms.push(platform);
+
+            //Creating shrooms from camera position so its not tied to the character
+            var shroomH = random(20, 80);
+            var shroom = createSprite(camera.position.x+width/2+random(200, 400), GROUND_Y-130, 50, 60);
+            shroom.setCollider("circle")
+            shroom.addImage(shroomImg);
+            shroom.debug = true;
+            shrooms.push(shroom);
+
+            //Creating small enemies between the shrooms with a basic velocity 1;
+            var small_enemy = createSprite(shroom.position.x+random(40, 50), GROUND_Y-110, 50, 50);
+            small_enemy.addImage(enemy_smallImg);
+            small_enemy.velocity.x = 1;
+            small_enemy.setCollider("circle", 0, 0, 20, 20)
+            small_enemy.debug = true;
+            enemies.push(small_enemy);
+            //            console.log(platform)
         }
     }
-    
-    //Platform collision
-    
-       warrior.collide(platforms);
-        
+
+    //Collecting the coins
+
+    ////the first parameter will be the sprite (individual or from a group) 
+    //calling the function
+    //the second parameter will be the sprite (individual or from a group)
+    //against which the overlap, collide, bounce, or displace is checked
+    warrior.overlap(coins, collect);
+
+
+    //Platform-warrior and warrior-shroom collision
+    warrior.collide(platforms);
+    //    warrior.collide(shrooms);
+
+    //Warrior and enemy collision + life lost
+    //    for(var i = 0; i < enemies.length; i++){
+    //        var damage = warrior.collide(enemies[i]);
+    //        if(damage){
+    //            lifePoints = lifePoints - 10;
+    //        }
+    //    }
+    //
+    //    if(lifePoints == 0){
+    //        dead();
+    //    }
+
     //Enemies movement
     //Between two shrooms. If they collide the speed changes by *-1;
-    for(var i = 0; i < enemies.length; i++){
-        for(var j = 0; j < shrooms.length; j++){
-            if(enemies[i].collide(shrooms[j])){
-            enemies[i].velocity.x *= -1;
+        for(var i = 0; i < enemies.length; i++){
+            for(var j = 0; j < shrooms.length; j++){
+                if(enemies[i].collide(shrooms[j])){
+                    enemies[i].velocity.x *= -1;
+                }
+            }  
         }
-        }
-        
-    }
-    
-    
-   
+
+
     //Centering camera position
     camera.position.x = warrior.position.x + width/4;
-   
-    
-   
+
     //Wrapping ground
     if(camera.position.x > ground.position.x-ground.width+width/2){
-//        var ground_new = createSprite(ground.position.x+width/2, GROUND_Y);
-//        ground_new.addImage(groundImg);
-//        grounds.push(ground_new);
         ground.position.x+=ground.width;
-        
     }
-    
+
     //Drawing background and background Image aswell
     background(200); 
     camera.off();
     image(backgroundImg, 0, GROUND_Y-height);
     camera.on();
-    
-     text('Game score: ' + score, warrior.position.x +200, 100);
+
+    //Game score and life points
+    textSize(14);
+    textStyle(BOLD);
+    text('Game score: ' + score, warrior.position.x +200, 100);
+    text('Life Points: ' + lifePoints, warrior.position.x +200, 200);
+
+
     //Drawing sprites
+    drawingSprites();
+}
+
+function collect(collector, collected){
+    collected.remove();
+    score += 10;
+}
+
+function dead(){
+    updateSprites(false);
+    gameOver = true;
+}
+function drawingSprites(){
+
     drawSprites(coins);
     drawSprite(ground);
     drawSprite(warrior);
@@ -188,21 +223,22 @@ function draw()
     drawSprites(platforms);
     drawSprites(enemies);
     drawSprites(shrooms);
+
 }
 
 function newGame() {
 
-  gameOver = false;
-  updateSprites(true);
-  warrior.position.x = width/2;
-  warrior.position.y = GROUND_Y - 130;
-  warrior.velocity.y = 0;
-  ground.position.x = width/2;
-  ground.position.y = GROUND_Y;
+    gameOver = false;
+    updateSprites(true);
+    warrior.position.x = width/2;
+    warrior.position.y = GROUND_Y - 130;
+    warrior.velocity.y = 0;
+    ground.position.x = width/2;
+    ground.position.y = GROUND_Y;
 }
 
 function mousePressed() {
-  if(gameOver)
-    newGame();
-//  warrior.velocity.y = FLAP;
+    if(gameOver)
+        newGame();
+    //      warrior.velocity.y = FLAP;
 }
