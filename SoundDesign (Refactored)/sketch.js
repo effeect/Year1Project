797be9aac6e75d9aa325cf118145e259159748f7
effeect <oliver.dimes3@gmail.com>
2018-05-10 14:53:39 +0100

    Reactiny Sky
    WIP

--- SoundDesign (Refactored)/sketch.js
+++ SoundDesign (Refactored)/sketch.js
@@ -240,7 +240,7 @@ function draw()
 //    camera.on();
 
     //Game score and life points
-    background(255)
+    background(bassMapped * 2 + 100,255,255)
     textSize(20);
     textStyle(BOLD);
     stroke(0)
